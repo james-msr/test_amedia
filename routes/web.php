63b98493e5dff02b6158b13369ec2fff464ebe9a
james-msr <smr01234585@gmail.com>
2022-04-25 22:19:17 +0500

    added auth middleware

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,11 @@ Route::post('/register', [RegisterController::class, 'register'])->name('registe
 Route::get('/login', [LoginController::class, 'loginForm'])->name('login.form');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 
-Route::get('/application/create', [ApplicationController::class, 'create'])->name('application.create');
-Route::post('/application/store', [ApplicationController::class, 'store'])->name('application.store');
+Route::middleware('auth')->group(function () {
+    Route::get('/application/create', [ApplicationController::class, 'create'])->name('application.create');
+    Route::post('/application/store', [ApplicationController::class, 'store'])->name('application.store');
 
-Route::get('/applications', [ApplicationController::class, 'index'])->name('application.index');
-Route::get('/applications/data', [ApplicationController::class, 'applicationData']);
-Route::post('/application/check', [ApplicationController::class, 'check']);
+    Route::get('/applications', [ApplicationController::class, 'index'])->name('application.index');
+    Route::get('/applications/data', [ApplicationController::class, 'applicationData']);
+    Route::post('/application/check', [ApplicationController::class, 'check']);
+});
